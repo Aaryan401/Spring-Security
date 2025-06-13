1abfe204932a7f6a4feadcfda9a186b60fe183fa
Aaryan <aaryanuttam54321@gmail.com>
2025-06-13 23:38:36 +0530

    created task repository

---
+++ src/main/java/com/example/Spring_Security/Repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.example.Spring_Security.Repository;
+
+import com.example.Spring_Security.Entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository <Task, Long> {
+}
